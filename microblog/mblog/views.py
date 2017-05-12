6ce185048d3a7031cefea233866f954645b0818a
Steven <banguijun@sina.com>
2017-05-12 18:36:20 +0800

    添加输入表单
    加入输入表单。存在的问题：表单提交不能保存。

--- microblog/mblog/views.py
+++ microblog/mblog/views.py
@@ -6,6 +6,10 @@ from .forms import UpdateMBForm
 # Create your views here.
 
 def home(request) : 
+    form = UpdateMBForm(request.POST)
+    # if form.is_valid : 
+        #mblog = MBlog.objects.get_or_create(content=request.POST.get('content'))
+
     mblogs = MBlog.objects.all()
     return render(request, 'home.html', \
         {'form' : UpdateMBForm, \
