eb815391e069b85e2e34296d1a5401667ab8f319
Steven <banguijun@sina.com>
2017-05-25 18:10:10 +0800

    修改admin中的显示方式
    修改admin中的显示方式

--- microblog/mblog/admin.py
+++ microblog/mblog/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import MBlog
 class MBlogAdmin(admin.ModelAdmin) : 
-    list_display = ('content', )
+    list_display = ('__unicode__', 'content', 'time')
 
 admin.site.register(MBlog, MBlogAdmin)
--- microblog/mblog/models.py
+++ microblog/mblog/models.py
@@ -6,8 +6,8 @@ from django.db import models
 # Create your models here.
 
 class MBlog(models.Model) : 
-    content = models.TextField(default = u'没有内容')
-    time = models.DateTimeField(auto_now = True)
+    content = models.TextField(default = u'没有内容', verbose_name = u'内容')
+    time = models.DateTimeField(auto_now = True, verbose_name = u'时间')
 
     def __unicode__(self) : 
-        return self.content[0:5]
+        return self.content[0:5] + '...'
